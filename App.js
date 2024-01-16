114664d95bc642a75a3e94baa05461c657d69d92
Hanna Zhou <zhou.hanyi@northeastern.edu>
2024-01-16 14:57:07 -0800

    added Input component

--- App.js
+++ App.js
@@ -1,29 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
-import { useState } from "react";
+import Input from "./components/Input";
 
 export default function App() {
   const appName = "My awesome app";
-  const [text, setText] = useState(""); // array destructuring
-
-  // callback handler
-  function changeTextHandler(changeText) {
-    console.log("user is typing", changeText);
-    setText(changeText);
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} version={2} />
-      <TextInput
-        placeholder="Type something"
-        style={styles.input}
-        value={text}
-        onChangeText={changeTextHandler}
-      />
-      <Text>{text}</Text>
+      <Input />
     </View>
   );
 }
@@ -34,10 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  input: {
-    borderBottomWidth: 2,
-    borderBottomColor: "purple",
-    width: 105,
   },
 });
